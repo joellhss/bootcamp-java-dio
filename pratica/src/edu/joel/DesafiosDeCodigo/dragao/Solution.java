f86c88e418711a83fe271aa716860519b0dd5b52
Joel Henrique <joelhenrique78@gmail.com>
2023-03-17 10:55:34 -0300

    Finalizando primeira série de desafios

---
+++ pratica/src/edu/joel/DesafiosDeCodigo/dragao/Solution.java
@@ -0,0 +1,24 @@
+package edu.joel.DesafiosDeCodigo.dragao;
+
+import java.util.*;
+public class Solution{
+    public static void main(String[] args){
+        int casos, poderDeLuta;
+
+        Scanner ler = new Scanner(System.in);
+
+        casos = ler.nextInt();
+
+        for(int i = 0; i < casos; i++){
+            poderDeLuta = ler.nextInt();
+
+//TODO: Implemente a condição adequada para a impressão dos textos conforme o solicitado no desafio:
+
+            if(poderDeLuta <= 8000){
+                System.out.println("Inseto!");
+            } else {
+                System.out.println("Mais de 8000!");
+            }
+        }
+    }
+}
---
+++ pratica/src/edu/joel/DesafiosDeCodigo/somaDeHComNTermos/Main.java
@@ -0,0 +1,19 @@
+package edu.joel.DesafiosDeCodigo.somaDeHComNTermos;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] Args) {
+
+        double h = 0;
+        Scanner sc = new Scanner(System.in);
+        double n = sc.nextDouble();
+
+        for (int i = 1; i <= n; i++) {
+            h += 1.0/i;
+        }
+        System.out.println(Math.round(h));
+        //TODO: Imprima a soma dos termos, considerando um resultado com duas casas decimais:
+
+    }
+}
